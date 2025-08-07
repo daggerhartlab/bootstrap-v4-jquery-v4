3f1cc99ae9068b4a564e04266c8be46772500d03
Jonathan Daggerhart <jonathan@daggerhartlab.com>
2025-08-07 15:32:29 -0400

    Attempting js lint fixes

--- js/src/jquery.deprecated_functions.js
+++ js/src/jquery.deprecated_functions.js
@@ -7,50 +7,50 @@ import $ from 'jquery'
 
 /** CONSTANTS AND VARIABLES * */
 
-const class2type = {};
+const class2type = {}
 
-const toString = class2type.toString;
+const { toString } = class2type
 
 // Populate the class2type map.
-jQuery.each(
+$.each(
   'Boolean Number String Function Array Date RegExp Object Error Symbol'.split(
-    ' ',
+    ' '
   ),
-  function (i, name) {
-    class2type[`[object ${name}]`] = name.toLowerCase();
-  },
-);
+  (i, name) => {
+    class2type[`[object ${name}]`] = name.toLowerCase()
+  }
+)
 
-jQuery.fx.interval = 13;
+$.fx.interval = 13
 
 /**
  * Takes a well-formed JSON string and returns the resulting JavaScript value.
  * As of jQuery 3.0, $.parseJSON is deprecated. To parse JSON strings use the native JSON.parse method instead.
  */
-/* eslint-disable-next-line no-useless-escape */
+
 const rvalidtokens =
-  /(,)|(\[|{)|(}|])|"(?:[^"\\\r\n]|\\["\\/bfnrt]|\\u[\da-fA-F]{4})*"\s*:?|true|false|null|-?(?!0\d)\d+(?:\.\d+|)(?:[eE][+-]?\d+|)/g;
+  /(,)|(\[|{)|(}|])|"(?:[^\n\r"\\]|\\["/\\bfnrt]|\\u[\dA-Fa-f]{4})*"\s*:?|true|false|null|-?(?!0\d)\d+(?:\.\d+|)(?:[Ee][+-]?\d+|)/g
 
 /** FUNCTIONS * */
 
 $.fn.isFunction = function (fn) {
-  return typeof fn === 'function';
-};
+  return typeof fn === 'function'
+}
 
 $.isFunction = function (obj) {
-  return typeof obj === 'function';
-};
+  return typeof obj === 'function'
+}
 
 $.type = function (obj) {
-  if (obj == null) {
-    return `${obj}`;
+  if (obj === null) {
+    return `${obj}`
   }
 
   // Support: Android <=2.3 only (functionish RegExp)
-  return typeof obj === 'object' || typeof obj === 'function'
-    ? class2type[toString.call(obj)] || 'object'
-    : typeof obj;
-};
+  return typeof obj === 'object' || typeof obj === 'function' ?
+    class2type[toString.call(obj)] || 'object' :
+    typeof obj
+}
 
 /**
  * Remove the whitespace from the beginning and end of a string.
@@ -63,117 +63,116 @@ $.type = function (obj) {
  * More information: https://api.jquery.com/jQuery.trim/
  */
 
-  // Support: Android<4.1, IE<9
-  // Make sure we trim BOM and NBSP
-const rtrim = /^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g;
+// Support: Android<4.1, IE<9
+// Make sure we trim BOM and NBSP
+const rtrim = /^\s+|\s+$/g
 
 // Support: Android<4.1, IE<9
-$.trim = (text) => {
-  return text == null ? '' : `${text}`.replace(rtrim, '');
-};
+$.trim = text => {
+  return text === null ? '' : `${text}`.replace(rtrim, '')
+}
 
 /* Determine whether the argument is an array.
  * This API has been deprecated in jQuery 3.2; please use the native Array.isArray method instead.
  * MOre information: https://api.jquery.com/jQuery.isArray/ */
-$.isArray = (obj) => {
-  return jQuery.type(obj) === 'array';
-};
+$.isArray = obj => {
+  return jQuery.type(obj) === 'array'
+}
 
 /* The $.camelCase() function in jQuery converts a hyphenated string to
  * camel case. It is used internally by jQuery for manipulating CSS
  * properties and data attributes.  */
 // Matches dashed string for camelizing
-const rmsPrefix = /^-ms-/;
-const rdashAlpha = /-([\da-z])/gi;
+const rmsPrefix = /^-ms-/
+const rdashAlpha = /-([\da-z])/gi
 
 // Used by jQuery.camelCase as callback to replace()
 $.fcamelCase = (all, letter) => {
-  return letter.toUpperCase();
-};
+  return letter.toUpperCase()
+}
 
-$.camelCase = (string) => {
-  return string.replace(rmsPrefix, 'ms-').replace(rdashAlpha, $.fcamelCase);
-};
+$.camelCase = string => {
+  return string.replace(rmsPrefix, 'ms-').replace(rdashAlpha, $.fcamelCase)
+}
 
 /** The jQuery.isWindow() function is a function used to determine if
  * a given argument is a window object. It returned true if the
  * object was a window and false otherwise. This function was
  * deprecated in jQuery version 3.3 and has been removed */
-$.isWindow = (obj) => {
+$.isWindow = obj => {
   /* jshint eqeqeq: false */
-  return obj != null && obj === obj.window;
-};
+  return obj !== null && obj === obj.window
+}
 
 /* This function was to check nodes names, but it
  * was documented: https://github.com/jquery/jquery/issues/3475 */
 $.nodeName = (elem, name) => {
-  return elem.nodeName && elem.nodeName.toLowerCase() === name.toLowerCase();
-};
+  return elem.nodeName && elem.nodeName.toLowerCase() === name.toLowerCase()
+}
 
 /* The $.isNumeric() method checks whether its argument represents a numeric
  * value. If so, it returns true. Otherwise it returns false.
  * The argument can be of any type. */
-$.isNumeric = (obj) => {
-  return !Number.isNaN(parseFloat(obj)) && Number.isFinite(obj);
-};
+$.isNumeric = obj => {
+  return !Number.isNaN(parseFloat(obj)) && Number.isFinite(obj)
+}
 
 /* Return a number representing the current time. */
 /* This API has been deprecated in jQuery 3.3; please use the native Date.now() method instead. */
-jQuery.now = function () {
-  return new Date().getTime();
-};
+$.now = function () {
+  return Date.now()
+}
 
-jQuery.parseJSON = function (data) {
+$.parseJSON = function (data) {
   // Attempt to parse using the native JSON parser first
   if (window.JSON && window.JSON.parse) {
     // Support: Android 2.3
     // Workaround failure to string-cast null input
-    return window.JSON.parse(`${data}`);
+    return window.JSON.parse(`${data}`)
   }
 
-  let requireNonComma;
-  let depth = null;
+  let requireNonComma
+  let depth = null
   /* eslint-disable-next-line no-jquery/no-trim */
-  const str = jQuery.trim(`${data}`);
+  const str = $.trim(`${data}`)
 
   // Guard against invalid (and possibly dangerous) input by ensuring that nothing remains
   // after removing valid tokens
 
   return str &&
   /* eslint-disable-next-line no-jquery/no-trim */
-  !jQuery.trim(
-    str.replace(rvalidtokens, function (token, comma, open, close) {
+  !$.trim(
+    str.replace(rvalidtokens, (token, comma, open, close) => {
       // Force termination if we see a misplaced comma
       if (requireNonComma && comma) {
-        depth = 0;
+        depth = 0
       }
 
       // Perform no more replacements after returning to outermost depth
       if (depth === 0) {
-        return token;
+        return token
       }
 
       // Commas must not follow "[", "{", or ","
-      requireNonComma = open || comma;
+      requireNonComma = open || comma
 
       // Determine new depth
       // array/object open ("[" or "{"): depth += true - false (increment)
       // array/object close ("]" or "}"): depth += false - true (decrement)
       // other cases ("," or primitive): depth += true - true (numeric cast)
-      depth += !close - !open;
+      depth += !close - !open
 
       // Remove this token
-      return '';
-    }),
-  )
-    ? /* eslint-disable-next-line no-new-func */
-    Function(`return ${str}`)()
-    : jQuery.error(`Invalid JSON: ${data}`);
-};
+      return ''
+    })
+  ) ? /* eslint-disable-next-line no-new-func */
+    new Function(`return ${str}`)() :
+    $.error(`Invalid JSON: ${data}`)
+}
 
 /* OBJECTS */
 
-jQuery.extend({
+$.extend({
   /* cssNumber is an object containing all CSS properties that may be used without a
 unit. The .css() method uses this object to see if it may append
 px to unitless values.
@@ -191,7 +190,7 @@ More information about it and how to use it here: https://jqapi.com/jquery.cssnu
     orphans: true,
     widows: true,
     zIndex: true,
-    zoom: true,
+    zoom: true
   },
 
   /**
@@ -200,6 +199,6 @@ More information about it and how to use it here: https://jqapi.com/jquery.cssnu
    * particularly older ones. I set it to 'stylefloat' by default */
   cssProps: {
     // normalize float css property
-    float: 'styleFloat',
-  },
-});
+    float: 'styleFloat'
+  }
+})
